6827fa0d6f4455c30d63b136b3f98d239945ef70
VishakaVarmaVimal <varmavimal.v@husky.neu.edu>
2020-01-27 03:43:40 -0500

    Testing circleCI mySQL

--- sql-data/create.sql
+++ sql-data/create.sql
@@ -6,7 +6,8 @@ CREATE TABLE `UsersData` (
   `last_name` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `email_address` varchar(100) NOT NULL,
-  `account_created` timestamp(6) NOT NULL DEFAULT '0000-00-00 00:00:00.000000',
-  `account_updated` timestamp(6) NOT NULL DEFAULT '0000-00-00 00:00:00.000000',
+  `account_created` timestamp(6) NULL DEFAULT NULL,
+  `account_updated` timestamp(6) NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
+
