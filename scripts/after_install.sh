500441cf262ed2a8b913550cc3c886acd5dbe394
VishakaVarmaVimal <varmavimal.v@husky.neu.edu>
2020-03-10 20:25:23 -0400

    Fixing env

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,4 +5,12 @@ cd /home/ubuntu/webapp
 npm cache clean --force
 sudo rm -rf node_modules
 sudo rm -rf package-lock.json
+cd ..
+sudo mkdir webapp/var
+cd ..
+sudo cp /var/.env /home/ubuntu/webapp/var
+cd /home/ubuntu/webapp/var/
+sudo chmod 666 .env
+cd ..
+cd /home/ubuntu/webapp
 npm install
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /home/ubuntu/webapp
-sudo npm install forever -g
+#sudo npm install forever -g
 sudo pkill -f node
 fuser -k 3000/tcp
-forever start server.js
+#sudo --preserve-env=DB_PORT,DB_HOST,DB_USER,DB_PASSWORD,DB_NAME,S3_BUCKET printenv
+#sudo --preserve-env=DB_PORT,DB_HOST,DB_USER,DB_PASSWORD,DB_NAME,S3_BUCKET forever start server.js
+nodemon server.js
