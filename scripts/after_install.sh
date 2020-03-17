50bc106498331abae9f2343313affb01a1f04758
VishakaVarmaVimal <varmavimal.v@husky.neu.edu>
2020-03-16 21:37:10 -0400

    assignment8 V1

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,6 +5,7 @@ cd /home/ubuntu/webapp
 npm cache clean --force
 sudo rm -rf node_modules
 sudo rm -rf package-lock.json
+sudo cp configs/cloudwatch-config.json /opt/cloudwatch-config.json
 sudo cp /var/.env /home/ubuntu/webapp/
 sudo chmod 666 /home/ubuntu/webapp/.env
 cd /home/ubuntu/webapp
